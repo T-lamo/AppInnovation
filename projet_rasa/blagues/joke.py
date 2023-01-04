1ec204221a6b4088298170cbc32a0682d7dc144c
rabarisoa-faraniaina <frabarisoa@gmail.com>
2023-01-04 17:54:12 +0100

    add jokes

---
+++ projet_rasa/blagues/joke.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_joke():
+  url = "https://blague.xyz/api/joke/random"
+  headers = { "Accept": "application/json" }
+  response = requests.get(url, headers=headers)
+  if response.status_code == 200:
+    joke = response.json()["joke"]
+    return str(joke["question"]) +" "+ str(joke["answer"])
+  else:
+    return None
+
+joke = get_joke()
+if joke:
+  print(joke)
+else:
+  print("Désolée je n'ai pas trouvé de blagues pour vous aujourd'hui haha")
